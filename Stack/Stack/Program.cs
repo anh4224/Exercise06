1426e3c2c702686416d1b9e1c9aa3ee574b4c11b
quanganh <bantia447@gmail.com>
2019-10-24 17:52:44 +0700

    Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Pham" , "Ngoc" , "Duy" };
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array[i] + " ");
+            }
+            //for (int i = 0; i < array.Length; i++)
+                //Console.Write(array[i] + " ");
+        }     
+    }
+}
---
+++ Stack/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stack;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPush()
+        {
+            MyStack S = new MyStack();
+            S.Push("1");
+            S.Push("2");
+            S.Push("3");
+            Assert.AreEqual(3, S.Count());
+        }
+        [TestMethod]
+        public void TestPop()
+        {
+            MyStack S = new MyStack();
+            S.Push("1");
+            S.Push("2");
+            S.Push("3");
+            Assert.AreEqual("3", S.Pop());
+            Assert.AreEqual("2", S.Pop());
+        }
+        [TestMethod]
+        public void TestPeek()
+        {
+            MyStack S = new MyStack();
+            S.Push("1");
+            S.Push("2");
+            S.Push("3");
+            Assert.AreEqual("3", S.Peek());
+            Assert.AreEqual(3, S.Count());
+        }
+        [TestMethod]
+        public void TestClear()
+        {
+            MyStack S = new MyStack();
+            S.Push("1");
+            S.Push("2");
+            S.Push("3");
+            S.Clear();
+            Assert.AreEqual(0, S.Count());
+        }
+    }
+}
